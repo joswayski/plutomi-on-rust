53d7636c317d67e9815aeaeeb4bd4fd66833d701
Jose Valerio <contact@josevalerio.com>
2023-09-17 03:53:59 -0400

    moar

--- projects/hello_cargo/target/debug/hello_cargo.d
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -1,1 +1,1 @@
-/Users/josevalerio/Documents/GitHub/plutomirs/hello_cargo/target/debug/hello_cargo: /Users/josevalerio/Documents/GitHub/plutomirs/hello_cargo/src/main.rs
+/Users/josevalerio/Documents/GitHub/plutomirs/projects/hello_cargo/target/debug/hello_cargo: /Users/josevalerio/Documents/GitHub/plutomirs/projects/hello_cargo/src/main.rs
